3caa8a82b6d3f84406672c77724be8b3ec69da1f
Samuel Freundel <samuel.freundel@uni-ulm.de>
2026-07-19 22:18:40 +0200

    feat: implementing PasswordHash as wrapper of a hashed password

--- backend/src/modules/accounts/domain/mod.rs
+++ backend/src/modules/accounts/domain/mod.rs
@@ -1,5 +1,10 @@
-pub mod email;
-mod user_id;
+mod email;
+pub use email::Email;
 
+mod user_id;
 pub use user_id::UserId;
+
+mod password_hash;
+pub use password_hash::PasswordHash;
+
 pub mod user;
