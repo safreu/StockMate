3f7da2bf77d0edca70f24c7c553ced601a919e07
Samuel Freundel <samuel.freundel@uni-ulm.de>
2026-08-18 18:09:01 +0200

    feat: Implementing domain layer for device and associated structs

---
+++ backend/src/modules/devices/domain/mod.rs
@@ -0,0 +1,11 @@
+mod device_id;
+pub use device_id::DeviceId;
+
+mod device_kind;
+pub use device_kind::{DeviceKind, DeviceKindError};
+
+mod device_name;
+pub use device_name::{DeviceName, DeviceNameError};
+
+mod device;
+pub use device::Device;
